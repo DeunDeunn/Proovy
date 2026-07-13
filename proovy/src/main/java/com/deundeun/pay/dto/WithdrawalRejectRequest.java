95070976c32a5f1b9a7be2904a711322c74e9949
cochae <ycw1203@naver.com>
2026-07-13 22:48:24 +0900

    🐛 Fix: 출금 신청/반려 요청 DTO에 Jackson 역직렬화용 생성자 추가
    @Builder만 있고 명시적 생성자가 없어 Lombok이 기본 생성자를 안 만들어주는
    바람에, Jackson이 WithdrawalApplyRequest/WithdrawalRejectRequest를 만들
    방법이 없어 실제 요청이 전부 실패하던 문제 수정 (@NoArgsConstructor,
    @AllArgsConstructor 추가). Spring의 실제 ObjectMapper로 역직렬화 검증 완료.

--- proovy/src/main/java/com/deundeun/pay/dto/WithdrawalRejectRequest.java
+++ proovy/src/main/java/com/deundeun/pay/dto/WithdrawalRejectRequest.java
@@ -2,11 +2,15 @@ package com.deundeun.pay.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WithdrawalRejectRequest {
     @NotBlank(message = "반려 사유를 입력해주세요.")
     @Size(max = 255, message = "반려 사유는 255자 이내로 입력해주세요.")
