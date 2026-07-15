9ab0e142ecaa0df77dd90cb3b6efd3d7387ee226
jmopkj <jmopkj23@gmail.com>
2026-07-15 11:10:36 +0900

    🐛 Fix: AI 검수규칙 요청 바인딩 수정 및 SQL문 수정

--- proovy/src/main/java/com/deundeun/ai/dto/AiReviewRuleRequest.java
+++ proovy/src/main/java/com/deundeun/ai/dto/AiReviewRuleRequest.java
@@ -1,5 +1,7 @@
 package com.deundeun.ai.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,4 +12,14 @@ public class AiReviewRuleRequest {
 
     private String reviewMode;
     private String ruleText;
+
+    @JsonCreator
+    public AiReviewRuleRequest(
+            @JsonProperty("reviewMode") String reviewMode,
+            @JsonProperty("ruleText") String ruleText
+    ) {
+        this.reviewMode = reviewMode;
+        this.ruleText = ruleText;
+    }
+
 }
