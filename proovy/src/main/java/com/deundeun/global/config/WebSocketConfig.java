b204b0564369ffaf75a668436b742aa43d0c911c
ABCganada <shinmg9712@gmail.com>
2026-07-15 11:45:43 +0900

    ✨ Feat: STOMP 핸드셰이크 시 쿠키 기반 JWT 인증

--- proovy/src/main/java/com/deundeun/global/config/WebSocketConfig.java
+++ proovy/src/main/java/com/deundeun/global/config/WebSocketConfig.java
@@ -1,5 +1,8 @@
 package com.deundeun.global.config;
 
+import com.deundeun.global.websocket.CustomHandshakeHandler;
+import com.deundeun.global.websocket.JwtHandshakeInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -7,12 +10,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
+        registry
+            .addEndpoint("/ws")
+            .addInterceptors(jwtHandshakeInterceptor)
+            .setHandshakeHandler(new CustomHandshakeHandler());
     }
 
     @Override
