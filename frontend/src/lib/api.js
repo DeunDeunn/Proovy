d27bd406b7caddad1ede0ab7d1355d90330a519c
Jungjaeyeong <jjy8037@gmail.com>
2026-07-16 19:28:28 +0900

    ✨ Feat: axios 공통 클라이언트 및 React Query Provider 세팅

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api",
+  withCredentials: true, // 요청에 쿠키 포함
+});
+
+// 응답 인터셉터
+api.interceptors.response.use(
+  (response) => response.data.data,
+  (error) => {
+    const body = error.response?.data;
+    // 백엔드가 내려준 에러가 아니면 네트워크 오류
+    return Promise.reject(
+      body ?? { code: "NETWORK", message: "서버에 연결할 수 없습니다." },
+    );
+  },
+);
+
+export default api;
